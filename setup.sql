766a1821f2e16005712019fe8bc78e48812030c6
Urimus3600 <89651791+Urimus3600@users.noreply.github.com>
2025-12-01 18:25:38 -0500

    Update setup.sql
    removed procedure syntax errors

--- setup.sql
+++ setup.sql
@@ -1,3 +1,4 @@
+#DROP TABLE MEMBERS;
 CREATE TABLE Members ( 
 mid       INT PRIMARY KEY AUTO_INCREMENT,
 username  VARCHAR(50) NOT NULL UNIQUE,
@@ -12,7 +13,7 @@ CONSTRAINT chk_phone_required_for_non_needy
     OR (phone IS NOT NULL AND phone <> '')
   )
 );
-
+#DROP TABLE CARDS;
 CREATE TABLE Cards (
 mid INT NOT NULL,
 FOREIGN KEY (mid) REFERENCES Members(mid)
@@ -22,7 +23,7 @@ expdate DATE,
 zipcode INT,
 csv VARCHAR(6)
 );
-
+#DROP TABLE PLATES;
 CREATE TABLE Plates(
 pid           INT PRIMARY KEY AUTO_INCREMENT,
 display_order INT,
@@ -35,7 +36,7 @@ CONSTRAINT fk_plates_creator
   FOREIGN KEY (mid) REFERENCES Members(mid)
     ON DELETE RESTRICT ON UPDATE CASCADE
 );
-
+#DROP TABLE IMAGES;
 CREATE TABLE Images (
 pid           INT NOT NULL,
 display_order INT,
@@ -45,7 +46,7 @@ CONSTRAINT fk_images_plate
     ON DELETE CASCADE ON UPDATE RESTRICT,
 PRIMARY KEY (pid, display_order)
 );
-
+#DROP TABLE ON_SALE;
 CREATE TABLE On_Sale (
   pid         INT,
   quantity    INT NOT NULL,
@@ -55,7 +56,7 @@ CREATE TABLE On_Sale (
       ON DELETE RESTRICT ON UPDATE RESTRICT,
   PRIMARY KEY (pid, listed_at)
 );
-
+#DROP TABLE RESERVED;
 CREATE TABLE Reserved (
   pid         INT NOT NULL,
   mid         INT NOT NULL,
@@ -69,7 +70,7 @@ CREATE TABLE Reserved (
       ON DELETE RESTRICT ON UPDATE RESTRICT,
   PRIMARY KEY (pid, mid, reserved_at)
 );
-
+#DROP TABLE PURCHASED;
 CREATE TABLE Purchased (
   pid         INT NOT NULL,
   mid         INT NOT NULL,
@@ -83,7 +84,7 @@ CREATE TABLE Purchased (
       ON DELETE RESTRICT ON UPDATE RESTRICT,
   PRIMARY KEY (pid, mid, purchased_at)
 );
-
+#DROP TABLE PICKED_UP;
 CREATE TABLE Picked_up (
   pid         INT NOT NULL,
   mid         INT NOT NULL,
@@ -109,21 +110,29 @@ BEGIN
     FROM Plates P
     WHERE mid=P.mid;
 END;
+$$
 
+DELIMITER $$
 CREATE PROCEDURE Plates_On_Sale()
 BEGIN
 	SELECT *
     FROM Plates P, On_Sale OS
-    WHERE OS.quantity>0 AND P.pid = OS.pid
+    WHERE OS.quantity>0 AND P.pid = OS.pid;
 END;
+$$
 
+
+DELIMITER $$
 CREATE PROCEDURE Plates_Purchased()
 BEGIN
 	SELECT *
     FROM Plates P, Purchased B
-    WHERE OS.quantity>0 AND P.pid = B.pid
+    WHERE OS.quantity>0 AND P.pid = B.pid;
 END;
+$$
 
+DELIMITER $$
+#DROP PROCEDURE RESERVE;
 CREATE PROCEDURE Reserve(mid_in INT, pid_in INT)
 BEGIN
 	IF (EXISTS (SELECT * FROM Members M WHERE mid_in = M.mid AND M.m_type='customer'))
@@ -136,33 +145,39 @@ BEGIN
             VALUES (pid_in, mid_in, 1)
 			ON CONFLICT (pid, mid) DO UPDATE
 			SET quantity = quantity+1;
+		END IF;
     END IF;
 END;
+$$
 
+DELIMITER $$
 CREATE PROCEDURE Customer_Pick_Up(mid_in INT, pid_in INT)
-BEGIN
-	IF (EXISTS (SELECT * FROM Members M WHERE mid_in = M.mid AND M.m_type='customer'))
-		IF(NOT ValidatePaymentMethodExists(mid_in))
-            RETURN;
-		IF(EXISTS (SELECT * FROM Reserved R WHERE pid_in = R.pid AND mid_in = R.mid AND R.quantity>0))
+here:BEGIN
+	IF (EXISTS (SELECT * FROM Members M WHERE mid_in = M.mid AND M.m_type='customer')) THEN
+		IF (NOT ValidatePaymentMethodExists(mid_in)) THEN
+            LEAVE here;
+		ELSEIF(EXISTS (SELECT * FROM Reserved R WHERE pid_in = R.pid AND mid_in = R.mid AND R.quantity>0)) THEN
 			UPDATE Reserved
             SET quantity = quantity-1
             WHERE pid_in = pid;
             
             INSERT INTO Picked_up (pid, mid, quantity)
-            VALUES (pid_in, mid_in, 1)
+            VALUES (pid_in, mid_in, 1);
+		END IF;
 	ELSE
 	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Member is not a customer!';
     END IF;
 END;
+$$
 
+DELIMITER $$
 CREATE PROCEDURE Needy_Pick_Up(mid_in INT, pid_in INT)
-BEGIN
-	IF ((SELECT COUNT(*) FROM Members M WHERE mid = M.mid AND M.m_type='needy')>1)
-		IF(NOT StillNeedy(mid_in))
+here:BEGIN
+	IF ((SELECT COUNT(*) FROM Members M WHERE mid = M.mid AND M.m_type='needy')>1) then
+		IF(NOT StillNeedy(mid_in)) then
 			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Maximum plates (2) reached!';
-            RETURN;
-		ELSEIF(EXISTS (SELECT * FROM Purchased P WHERE pid_in = P.pid AND P.quantity>0))
+            LEAVE here;
+		ELSEIF(EXISTS (SELECT * FROM Purchased P WHERE pid_in = P.pid AND P.quantity>0)) then
 			UPDATE Purchased
             SET quantity = quantity-1
             WHERE pid_in = pid;
@@ -170,55 +185,60 @@ BEGIN
             UPDATE Reserved
 			SET quantity = quantity+1
             WHERE pid_in = pid;
+		end if;
     END IF;
 END;
-
+$$
+DELIMITER $$
 CREATE PROCEDURE Donate(mid_in INT, pid_in INT)
 BEGIN
-	IF (EXISTS (SELECT * FROM Members M WHERE mid_in = M.mid AND M.m_type='donor'))
-		IF(EXISTS (SELECT * FROM On_Sale OS WHERE pid_in = OS.pid AND OS.quantity>0))
+	IF (EXISTS (SELECT * FROM Members M WHERE mid_in = M.mid AND M.m_type='donor')) then
+		IF(EXISTS (SELECT * FROM On_Sale OS WHERE pid_in = OS.pid AND OS.quantity>0)) then
 			UPDATE On_Sale
             SET quantity = quantity-1
             WHERE pid_in = pid;
 			
             INSERT INTO Purchased (pid, mid, quantity)
             VALUES (pid_in, mid_in, 1)
-			ON CONFLICT (pid, mid) DO UPDATE
-			SET quantity = quantity+1;
+			ON DUPLICATE KEY UPDATE
+			quantity = quantity+1;
+		
+		END IF;
     END IF;
 END;
-
-CREATE PROCEDURE ValidatePaymentMethodExists(mid INT)
+$$
+DELIMITER $$
+CREATE function ValidatePaymentMethodExists(mid INT)
+returns bool
 BEGIN
     DECLARE total_cards INT;
     SELECT COUNT(*) INTO total_cards FROM Cards WHERE Cards.mid = mid;
 
     IF total_cards <= 0 THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No payment card provided for member!';
-        RETURN FALSE
-	ELSE RETURN TRUE
+        RETURN FALSE;
+	ELSE RETURN TRUE;
     END IF;
 END;
 $$
-DELIMITER ;
 
 DELIMITER $$
-
-CREATE PROCEDURE StillNeedy(mid INT)
+CREATE function StillNeedy(mid INT)
+returns bool
 BEGIN
     DECLARE total_reserved INT;
     DECLARE total_picked_up INT;
     SELECT SUM(R.quantity) INTO total_reserved FROM Reserved R WHERE R.mid = mid;
     SELECT SUM(P.quantity) INTO total_picked_up FROM Picked_Up P WHERE P.mid = mid;
     IF (total_reserved + total_picked_up) <= 1 THEN
-        SIGNAL SQLSTATE '01000' SET MESSAGE_TEXT = 'Still Needy!';
         RETURN TRUE;
 	ELSE
-		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Not Needy!';
         RETURN FALSE;
     END IF;
 END;
+$$
 
+DELIMITER $$
 CREATE TRIGGER On_Sale_Guard
 BEFORE INSERT ON On_Sale
 FOR EACH ROW
@@ -235,8 +255,9 @@ BEGIN
   THEN
     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Plate is already in another state';
   END IF;
-END$$
-
+END
+$$
+DELIMITER $$
 CREATE TRIGGER Reserved_Guard
 BEFORE INSERT ON Reserved
 FOR EACH ROW
@@ -257,8 +278,9 @@ BEGIN
   IF NOT EXISTS (SELECT 1 FROM On_Sale WHERE pid = NEW.pid LIMIT 1) THEN
     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot reserve a plate that is not On_Sale';
   END IF;
-END$$
-
+END
+$$
+DELIMITER $$
 CREATE TRIGGER Purchased_Guard
 BEFORE INSERT ON Purchased
 FOR EACH ROW
@@ -272,8 +294,9 @@ BEGIN
   IF EXISTS (SELECT 1 FROM Picked_up WHERE pid = NEW.pid LIMIT 1) THEN
     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Plate already picked up';
   END IF;
-END$$
-
+END
+$$
+DELIMITER $$
 CREATE TRIGGER Picked_up_Guard
 BEFORE INSERT ON Picked_up
 FOR EACH ROW
@@ -287,7 +310,6 @@ BEGIN
   IF EXISTS (SELECT 1 FROM Picked_up WHERE pid = NEW.pid LIMIT 1) THEN
     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Plate already picked up';
   END IF;
-END$$
-
+END
 $$
 DELIMITER ;
